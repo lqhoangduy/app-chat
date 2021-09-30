233f41ea4a21fe99b2629ec59597903e3f5d042c
hoangduylq <lqhoangduy@gmail.com>
2021-09-30 12:50:58 +0700

    change firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,15 +4,27 @@ import 'firebase/analytics';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAzyu2ixKRUQOHxTZEWksuwR4t22kxpdng',
+//   authDomain: 'chat-app-d6020.firebaseapp.com',
+//   projectId: 'chat-app-d6020',
+//   storageBucket: 'chat-app-d6020.appspot.com',
+//   messagingSenderId: '39752548726',
+//   appId: '1:39752548726:web:a0dd4dc609827bf508ba57',
+//   measurementId: 'G-BSQT09SFPR',
+// };
+
+//mail duyhoangpub@gmail.com pass duy1
 const firebaseConfig = {
-  apiKey: 'AIzaSyAzyu2ixKRUQOHxTZEWksuwR4t22kxpdng',
-  authDomain: 'chat-app-d6020.firebaseapp.com',
-  projectId: 'chat-app-d6020',
-  storageBucket: 'chat-app-d6020.appspot.com',
-  messagingSenderId: '39752548726',
-  appId: '1:39752548726:web:a0dd4dc609827bf508ba57',
-  measurementId: 'G-BSQT09SFPR',
+  apiKey: 'AIzaSyDYTHT3gvjdpMR9pAkEUxXWIKovReS9A_c',
+  authDomain: 'chat-app-97648.firebaseapp.com',
+  projectId: 'chat-app-97648',
+  storageBucket: 'chat-app-97648.appspot.com',
+  messagingSenderId: '356707814720',
+  appId: '1:356707814720:web:dfe6c1e00082649ffb3ea5',
+  measurementId: 'G-3KX54RWP14',
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
